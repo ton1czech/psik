34b2adee9a2c24216b2b914986e73aa504aa7a06
ton1czech <tonyasek007@gmail.com>
2021-06-16 13:55:18 +0200

    🎁NEW: checking for file type

--- src/psik.py
+++ src/psik.py
@@ -4,5 +4,13 @@ def organize():
     name = os.getlogin()
     os.chdir(f"/home/{name}/Downloads")
 
+    files = os.listdir()
+
+    for file in files:
+        if file.endswith(".html"):
+            print("Programming")
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+            print("Images")
+
 if __name__ == "__main__":
     organize()
