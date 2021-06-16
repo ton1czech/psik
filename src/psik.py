3c3bb4b9d3f049a99877ccd2c9e04f5197c46fb5
ton1czech <tonyasek007@gmail.com>
2021-06-16 16:25:01 +0200

    🎁NEW: permission to organize dir

--- src/psik.py
+++ src/psik.py
@@ -1,16 +1,25 @@
 import os
 
 def organize():
-    name = os.getlogin()
-    os.chdir(f"/home/{name}/Downloads")
+    accept = input("Are you sure you want to organize your Downloads directory? [y/N]: ")
+    if accept == "y" or accept == "Y" or accept == "yes":
+        accept = True
+    else:
+        print("All right\nshutting down...")
+        accept = False
 
-    files = os.listdir()
+    if accept == True:
+        print("Scanning the Downloads directory...")
+        name = os.getlogin()
+        os.chdir(f"/home/{name}/Downloads")
 
-    for file in files:
-        if file.endswith(".html"):
-            print("Programming")
-        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
-            print("Images")
+        files = os.listdir()
+
+        for file in files:
+            if file.endswith(".html"):
+                print("Programming")
+            if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                print("Images")
 
 if __name__ == "__main__":
     organize()
