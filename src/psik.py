e4d8e138259eae407a3d6b257e29362f38929986
ton1czech <tonyasek007@gmail.com>
2021-06-17 17:08:57 +0200

    🎁NEW: websites, python, pictures

--- src/psik.py
+++ src/psik.py
@@ -2,40 +2,53 @@ import os
 import shutil
 
 def organize():
+    count = 0
     accept = input("Are you sure you want to organize your Downloads directory? [y/N]: ")
     if accept == "y" or accept == "Y" or accept == "yes":
         print("Scanning the Downloads directory...")
         dir = f"/home/{os.getlogin()}"
         os.chdir(f"{dir}/Downloads")
+        files = os.listdir()
+        print(f"Found {len(files)} files.")
 
-        for file in os.listdir():
-            # Documents
-            if file.endswith(".html") or file.endswith(".js") or file.endswith(".ts"):
+        for file in files:
+            ## Documents ##
+            # websites #
+            if file.endswith(".html") or file.endswith(".js") or file.endswith(".ts") or file.endswith(".css") or file.endswith(".scss") or file.endswith(".php") or file.endswith(".jsx"):
                 temp_dir = f"{dir}/Documents/programming/websites"
                 if not os.path.exists(temp_dir):
                     os.makedirs(temp_dir)
                 shutil.move(file, f"{temp_dir}/{file}")
-                print("Website file moved")
+                count += 1
 
-            # Pictures
+            # python #
+            elif file.endswith(".py") or file.endswith(".pyw") or file.endswith(".pyx"):
+                temp_dir = f"{dir}/Documents/programming/python"
+                if not os.path.exists(temp_dir):
+                    os.makedirs(temp_dir)
+                shutil.move(file, f"{temp_dir}/{file}")
+                count += 1
+
+            ## Pictures ##
             elif file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
                 print("Picture file moved")
             
-            # Videos
-            elif file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".mkv"):
+            ## Videos ##
+            elif file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".mkv") or file.endswith(".webm") or file.endswith(".flv") or file.endswith("avi") or file.endswith(".m4v") or file.endswith(".mpg") or file.endswith(".mpeg"):
                 print("Video file moved")
             
-            # Musik
+            ## Musik ##
             elif file.endswith(".mp3"):
                 print("Musik file moved")
 
-            # Other
+            ## Other ##
             else:
                 temp_dir = f"{dir}/Documents/other"
                 if not os.path.exists(temp_dir):
                     os.makedirs(temp_dir)
                 shutil.move(file, f"{temp_dir}/{file}")
-                print("Other file moved")
+                count += 1
+        print(f"Downloads directory organized by psik.\nFiles moved: {count}")
     else:
         print("All right\nshutting down...")
         accept = False
